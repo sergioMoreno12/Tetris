45174b2ace81bf3b4929ddb2a7ba8ab7b7296fc7
Sergio <132287089+sergioMoreno12@users.noreply.github.com>
2023-10-14 14:53:39 +0200

    Update GameThread.java

--- GameThread.java
+++ GameThread.java
@@ -5,9 +5,16 @@ import java.util.logging.Logger;
 
 public class GameThread extends Thread {
     private GameArea ga;
+    private GameForm gf;
+    private int score;
+    private int level = 1; 
+    private int scorePerLevel=3;
+    private int pause = 1000;
+    private int speedUpPerLevel = 100;
     
-    public GameThread(GameArea ga){
+    public GameThread(GameArea ga, GameForm gf){
         this.ga = ga;
+       this.gf = gf;
     }
     
     
@@ -17,11 +24,25 @@ public class GameThread extends Thread {
             ga.spawnBlock();
            while(ga.moveBlockDown()){
              try {            
-            Thread.sleep(100);
+            Thread.sleep(pause);
         } catch (InterruptedException ex) {
             Logger.getLogger(GameThread.class.getName()).log(Level.SEVERE, null, ex);
         }  
       }
+           if(ga.isBlockOutOfBounds()){
+               System.out.println("Game Over");
+               break;
+           }
+           ga.moveBlockToBackground();
+           score +=ga.clearLines();
+           gf.updateScore(score);
+           int lvl = score / scorePerLevel + 1;
+           if(lvl > level){
+               level = lvl;
+               gf.updateLevel(level);
+               pause -= speedUpPerLevel;
+               
+           }
     }
   }
 }
